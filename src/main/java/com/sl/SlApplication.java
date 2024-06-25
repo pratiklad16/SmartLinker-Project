6af9e3d1cd85c89c4337b9c03deea70af53c24ae
Pratik Lad <pratiklad162003@gmail.com>
2024-06-25 13:48:27 +0530

    ex

--- src/main/java/com/sl/SlApplication.java
+++ src/main/java/com/sl/SlApplication.java
@@ -2,6 +2,7 @@ package com.sl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 
 @SpringBootApplication
 public class SlApplication {
